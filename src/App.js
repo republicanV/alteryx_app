eec4e7cc8484b3ed71780911020a291ae0d4c696
Vadim Sapovsky <vadimsapovsky@MacBook-Pro-Vadim.local>
2018-11-06 14:18:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'react-emotion';
+
+import Registration from './Registration';
+
+const Header = styled('header')`
+    display: flex;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 42px;
+    border-bottom: #29487D;
+    background-color: #4267B2;      
+`;
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <Registration/>
       </div>
     );
   }
---
+++ src/Registration.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import styled from 'react-emotion';
+import axios from 'axios';
+
+const RegisterForm = styled('form')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 80vh;
+    padding: 70px 0 0;
+    background-color: #E9EBEE;
+`;
+
+const InputError = styled('div')`
+  position: relative;
+  span {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 150px;
+    width: 370px;
+    padding: 10px;
+    border: 1px solid #6B84FC;
+    border-radius: 5px;
+    background-color: #e8ecff;
+    color: #323544;
+    text-align: center;
+  }
+`;
+
+const FormInput = styled('input')`
+    width: 230px;
+    height: 30px;
+    margin: 0 auto 12px;
+    padding: 8px 10px;
+    border: 1px solid #DDDFE2;
+    outline: none;
+    color: #1D2129;
+    font-size: 17px;
+`;
+
+const SignUpButton = styled('button')`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 230px;
+    padding: 0 16px;
+    border-radius: 2px;
+    outline: 0;
+    color: #FFF;
+    height: 34px;
+    cursor: pointer;
+    border: 1px solid;
+    border-color: #3B6E22 #3B6E22 #2C5115;
+    background-color: #69A74E;
+    box-shadow: inset 0 1px 1px #A4E388;
+    font-size: 19px;
+`;
+
+class Registration extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ submitted: true });
+
+        const newUser = { user: this.state.user };
+        axios.post('https://lab.lectrum.io/redux/api/user/6vf77z4hd5', newUser)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    render() {
+        const { user, submitted } = this.state;
+        return(
+            <RegisterForm onSubmit={this.handleSubmit}>
+                {submitted && !user.firstName && (
+                    <InputError>
+                        <span>Your first name should be at least 1 symbol long</span>
+                    </InputError>
+                )}
+                <FormInput
+                    name="firstName"
+                    value={user.firstName}
+                    placeholder="First name"
+                    onChange={this.handleChange}
+                />
+                {submitted && !user.lastName && (
+                    <InputError>
+                        <span>Your last name should be at least 1 symbol long</span>
+                    </InputError>
+                )}
+                <FormInput
+                    name="lastName"
+                    value={user.lastName}
+                    placeholder="Last name"
+                    onChange={this.handleChange}
+                />
+                {submitted && !user.email && (
+                    <InputError>
+                        <span>Please provide a valid email</span>
+                    </InputError>
+                )}
+                <FormInput
+                    name="email"
+                    value={user.email}
+                    placeholder="Email"
+                    onChange={this.handleChange}
+                />
+                {submitted && !user.password && (
+                    <InputError>
+                        <span>A password should be at least 5 symbols long</span>
+                    </InputError>
+                )}
+                <FormInput
+                    name="password"
+                    value={user.password}
+                    placeholder="password"
+                    onChange={this.handleChange}
+                />
+                <SignUpButton>Create Account</SignUpButton>
+            </RegisterForm>
+        );
+    }
+}
+
+export default Registration;
